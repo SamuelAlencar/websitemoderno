784b5effe54f8f8716c8e87a6b06c95c57aad05c
SamuelAlencar <samuel_nascimento@hotmail.com>
2022-06-26 14:34:34 -0300

    migraçao vercel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './login';
+import './css/tailwind.css';
+
+import reportWebVitals from "./reportWebVitals";
+ReactDOM.render(<Login/>,
+    document.getElementById('root')
+);
+
+reportWebVitals();
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+
+export default function Home() {
+
+  return (
+    <div className="container mx-auto py-4 justify-center flex">
+        <h1>LOGIN REALIZADO COM SUCESSO</h1>
+    </div>
+  );
+}
